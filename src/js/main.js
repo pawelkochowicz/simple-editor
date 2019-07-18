f474e6176f65c97023e73bf4babe46fda471f3d6
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2019-07-18 19:09:31 +0200

    feat:add functions in JS

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,14 @@
 "use strict";
 
-console.log(`Hello world!`);
+const text = document.querySelector('.form__text--js');
+const saveButton = document.querySelector('.save--js');
+const loadButton = document.querySelector('.load--js');
+
+saveButton.addEventListener('click', (e) => {
+    localStorage.setItem('text', text.value);
+});
+
+
+loadButton.addEventListener('click', () =>{
+text.value = localStorage.getItem('text')
+})
